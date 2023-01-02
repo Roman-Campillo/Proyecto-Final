a0c6103a6fcb2d4a342836c8ac5604fe3e2c7a54
Roman-Campillo <tumacat@gmail.com>
2023-01-02 16:20:16 +0000

    Primer commit

---
+++ proyectofinal/webapp/controller/CreateEmployee.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/m/UploadCollectionParameter"
+], function (Controller, MessageBox, UploadCollectionParameter) {
+    "use strict";
+
+
+    return Controller.extend("logaligroup.proyectofinal.controller.CreateEmployee", {
+    });
+
+});
---
+++ proyectofinal/webapp/controller/ShowEmployee.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
+], function (Controller,History) {
+	"use strict";
+
+    return Controller.extend("logaligroup.proyectofinal.controller.ShowEmployee", {
+    });
+
+});
